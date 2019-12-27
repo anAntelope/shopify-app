1452040e5e72b2f1094b43e5343fd570bfee7e78
matt <syan4@ualberta.ca>
2019-12-27 02:12:15 -0700

    test

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import { TextStyle } from '@shopify/polaris';
 const Index = () => (
     <div>
         <TextStyle variation="positive">
-            Sample app using React and Next. hello wow meow
+            Sample app using React and Next. hello hello
         </TextStyle>
     </div>
 );
